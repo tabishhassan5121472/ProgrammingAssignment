040e185e3b5c6f3654255097eabbcdc30e7807fd
tabishhassan5121472 <tabishhassan01998@gmail.com>
2022-02-12 12:41:12 +0500

    3rd

--- src/Components/Albums.js
+++ src/Components/Albums.js
@@ -3,7 +3,7 @@ import { Text, ScrollView } from 'react-native';
 const Albums = props => {
     return (
         <ScrollView>
-            <Text style={{ padding: 10, fontSize: 44, }}>{props.Album.title}</Text>
+            <Text style={{ padding: 10, fontSize: 24, }}>{props.Album.title}</Text>
         </ScrollView>
     );
 };
